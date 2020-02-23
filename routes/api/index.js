42318e30c20c75a93df6a703651f69aad8640fcc
Edward Harley <edharley19@gmail.com>
2020-02-23 10:01:07 -0500

    Update: Remove files not currently needed

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ var router = require('express').Router();
 
 router.use('/users', require('./users'));
 router.use('/trips', require('./trips'));
-// router.use('/lists', require('./lists'));
-// router.use('/todos', require('./todos'));
+
 
 module.exports = router;
